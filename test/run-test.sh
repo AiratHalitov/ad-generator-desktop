104c12db3fb901bd3686a93f3fdae459dfadfd2a
Airat Halitov <airat.halitov@gmail.com>
2020-08-04 20:10:58 +0500

    Add run-test.sh script

---
+++ test/run-test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+FILE=tst_test
+
+# Generate new Makefile
+#qmake
+
+# Clean all old project files
+make clean
+
+if [ -f "$FILE" ]; then
+    #cp $FILE $FILE.back
+    rm $FILE
+fi
+
+# Compile project (4 threads)
+make -j4
+
+echo -e "\n\n"
+
+# Run the program
+if [ -f "$FILE" ]; then
+    ./$FILE
+fi
